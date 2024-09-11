b4a5bd608963b6fe0761ffdfaeb28fea80506fad
reue <reue5118@protonmail.com>
2024-09-11 18:31:49 +1000

    [INIT]

--- calc_entropy.py
+++ calc_entropy.py
@@ -2,29 +2,29 @@ import re
 import math
 
 
-def calc_entropy(string: str) -> int:
+def calc_entropy(string: str) -> float:
     lower_case = len(re.findall(r"[a-z]", string))
     upper_case = len(re.findall(r"[A-Z]", string))
     digits = len(re.findall(r"[0-9]", string))
     special_chars = len(re.findall(r"[!-/:-@[-`{-~]", string))
 
-    lenght: list[int] = []
+    length: list[int] = []
     pool_size: list[int] = []
 
     if lower_case != 0:
-        lenght.append(lower_case)
+        length.append(lower_case)
         pool_size.append(26)
     if upper_case != 0:
-        lenght.append(upper_case)
+        length.append(upper_case)
         pool_size.append(26)
     if digits != 0:
-        lenght.append(digits)
+        length.append(digits)
         pool_size.append(10)
     if special_chars != 0:
-        lenght.append(special_chars)
+        length.append(special_chars)
         pool_size.append(32)
 
-    return round(math.log2(sum(pool_size) ** sum(lenght)), 3)
+    return round(math.log2(sum(pool_size) ** sum(length)), 3)
 
 
 if __name__ == "__main__":
